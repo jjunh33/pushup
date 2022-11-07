2e4ec01ab09e87cf49ea4e31c11322160b41fef8
jjunh33 <57091983+jjunh33@users.noreply.github.com>
2022-11-07 22:06:38 +0900

    Update pushup.py

--- pushup.py
+++ pushup.py
@@ -47,6 +47,7 @@ temp_cnt = 0
 temp_time = 0
 graph_y = []
 start_time = 0
+first = 0
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
@@ -56,7 +57,7 @@ with mp_pose.Pose(
         # print(vid_time)
         if vid_time != 0:
             end_time = vid_time
-            if vid_time - temp_time >= 5:
+            if vid_time - temp_time >= 5 and temp_time != 0:
                 print(temp_cnt)
                 graph_y.append(temp_cnt)
                 temp_cnt = 0
@@ -109,9 +110,7 @@ with mp_pose.Pose(
                         setup_time = time.time()
                     if (time.time() - setup_time) >= 3:
                         step = 0
-                        start_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-                        temp_time = start_time
-                        print(start_time)
+                        # toast("운동을 시작하세요!")
                 else:
                     setup_time = 0
             elif step == 0:
@@ -128,9 +127,13 @@ with mp_pose.Pose(
                 cv2.putText(image, "Step2. down", (820, 65), cv2.FONT_HERSHEY_PLAIN, 3,
                             (255, 255, 255), 5)
                 if arm_angle <= 90:
+                    if first == 0:
+                        start_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+                        temp_time = start_time
+                        print(start_time)
+                        first = 1
                     print("Step2 OK")
                     print("Step3. 팔 펴라")
-
                     step = 3
                 elif waist_angle < 150:
                     print("허리 피라고")
@@ -156,21 +159,22 @@ with mp_pose.Pose(
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
         # Flip the image horizontally for a selfie-view display.
-        # cv2.rectangle(image, )
         cv2.rectangle(image, (5, 880), (270, 930), (255, 0, 0), -1)
         cv2.putText(image, "count:%d" % cnt, (25, 920), cv2.FONT_HERSHEY_PLAIN, 3,
                     (255, 255, 255), 5)
-        # cv2.imshow('Pushup Counter', image)
+        cv2.imshow('Pushup Counter', image)
         out.write(image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
 total_time = int(end_time - start_time)
-graph_x = [i for i in range(5, total_time + 1, 5)]
+graph_x = [i for i in range(5, 5 * (total_time // 5 + (0 if total_time % 5 == 0 else 1)) + 1, 5)]
+if len(graph_x) > len(graph_y):
+    graph_y.append(cnt-sum(graph_y))
 print(graph_x)
 print(graph_y)
 cap.release()
 out.release()
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 plt.plot(graph_x, graph_y)
 # plt.show()
 plt.savefig("result.png")
